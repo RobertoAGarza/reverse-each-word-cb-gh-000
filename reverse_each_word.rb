c79776114f7fd7a82407a4c04e213106fa3aacf7
Roberto Garza <robertg4567@hotmail.com>
2018-07-19 17:57:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   myStringArray = string.split(" ")
   myStringArray.collect do |word|
     word.reverse
+    puts myStringArray.join(" ")
   end
   puts myStringArray.join(" ")
 end
